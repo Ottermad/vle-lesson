a5caf2ae4e5f0e29f835404e042e2e7107e0cf3c
Charles Thomas <charlie.thomas@attwoodthomas.net>
2017-12-20 21:48:47 +0000

    add subject detail, update, delete

--- app/subject/subject_functions.py
+++ app/subject/subject_functions.py
@@ -1,10 +1,11 @@
 from flask import jsonify, g
 
-from internal.exceptions import FieldInUseError, NotFoundError, UnauthorizedError
+from internal.exceptions import FieldInUseError
 from internal.helper import json_from_request, check_keys, get_record_by_id
 
 from app import db
 from .models import Subject
+from app.lesson.models import Lesson
 
 
 def create_subject(name, school_id):
@@ -43,3 +44,41 @@ def list_subject_view(request):
     # Get subjects and convert to dicts
     subjects = [s.to_dict() for s in subjects_for_school(g.user.school_id)]
     return jsonify({'success': True, 'subjects': subjects})
+
+
+def subject_detail_view(request, subject_id):
+    subject = get_record_by_id(subject_id, Subject)
+    return jsonify({'success': True, 'subject': subject.to_dict()})
+
+
+def subject_delete_view(request, subject_id):
+    subject = get_record_by_id(subject_id, Subject)
+
+    # Check if subject still has lessons
+    lesson = Lesson.query.filter_by(subject_id=subject.id).first()
+    if lesson is not None:
+        return jsonify({
+            'error': True,
+            'message': 'Please delete all lessons which are part of this subject'
+        }), 409
+    db.session.delete(subject)
+    db.session.commit()
+    return jsonify({'success': True, 'message': 'Deleted.'})
+
+
+def subject_update_view(request, subject_id):
+    # Get subject
+    subject = get_record_by_id(subject_id, Subject)
+
+    # Get JSON data
+    data = json_from_request(request)
+
+    # If name in data, then update the name
+    if 'name' in data.keys():
+        if subject_name_in_use(data['name'], school_id=subject.school_id):
+            raise FieldInUseError("name")
+        subject.name = data['name']
+
+    db.session.add(subject)
+    db.session.commit()
+    return jsonify({'success': True, 'message': 'Updated.'})
--- app/subject/views.py
+++ app/subject/views.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, g
 from .models import Subject
 from app.lesson.models import Lesson
-from .subject_functions import create_subject_view, list_subject_view
+from .subject_functions import (
+    create_subject_view, 
+    list_subject_view,
+    subject_detail_view,
+    subject_delete_view,
+    subject_update_view
+)
+from internal.exceptions import UnauthorizedError
 
 subject_blueprint = Blueprint("subject", __name__, url_prefix="/subject")
 
@@ -12,3 +19,18 @@ def subject_list_or_create_view():
         return create_subject_view(request)
     else:
         return list_subject_view(request)
+
+
+@subject_blueprint.route('/subject/<subject_id>', methods=['GET', 'PUT', 'DELETE'])
+def subject_individual_view(subject_id):
+    if request.method == 'GET':
+        return subject_detail_view(request, subject_id=subject_id)
+    if request.method == 'DELETE':
+        if g.user.has_permissions({'Administrator'}):
+            return subject_delete_view(request, subject_id)
+        raise UnauthorizedError()
+    if request.method == 'PUT':
+        if g.user.has_permissions({'Administrator'}):
+            return subject_update_view(request, subject_id)
+        raise UnauthorizedError()
+
